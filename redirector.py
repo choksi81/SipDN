32fcf4ebae35eecf077a006eabb79e82780431b5
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-26 22:42:16 -0500

    beginning work

---
+++ redirector.py
@@ -0,0 +1,12 @@
+
+# This module will answer a SIP INVITE with a 302 Moved Temporarily or 403 Forbidden
+# http://www.cisco.com/c/en/us/td/docs/voice_ip_comm/sip/proxies/1-0/administration/guide/ver1_0/appbcf.html
+
+import pjsua as pj
+LOG_LEVEL = 3
+
+class Redirector:
+    def __init__():
+        pass
+
+#:indentSize=4:tabSize=4:noTabs=true:wrap=soft:
